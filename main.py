ca56a6b50636a37b44a37f9514c05427a698e081
BuczynskiRafal <buczynski.rafal2@gmail.com>
2022-10-03 06:34:44 +0200

    extract data from links

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from pyswmm import Simulation
+from pyswmm import Simulation, Nodes, Links
 
 
-"""First option"""
-# sim = Simulation(inputfile='Site_Drainage_Model.inp')
-# sim.execute()
+"""
+1. Open SWMM file - done
+2. Get data about slope
+3. Get data about velocity
+4. Get data bout depth
+5. Prepare algorithm to calibration model
+    - validate velocity
+    - validate depth
+    - check if slope is minimal
+    - if not use regression or other algorithm to calibrate slope
+6. Insert new data to model
+7. Execute simulation again
+8. Show result in SWMM
+"""
 
-#
-# """Second option"""
-# with Simulation(inputfile='Site_Drainage_Model.inp') as sim:
-#     for step in sim:
-#         print(step.flow_routing_stats())
+with Simulation(inputfile='example.inp') as sim:
+    # print([link.linkid for link in Links(sim)])
+    for link in Links(sim):
+        # print(link.linkid)
+        print(link.inlet_node)
+        print(link.outlet_node)
--- training/1. Loading_model.py
+++
@@ -1,7 +0,0 @@
-from pyswmm import Simulation
-
-
-with Simulation(inputfile='Site_Drainage_Model.inp') as sim:
-    sim.step_advance(300)
-    for step in sim:
-        print(sim.current_time)
